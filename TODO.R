41acec731226a8ab8b8e7f85b04b6c54d9e9f017
Nikita Mozgunov <41270344+nikotinz@users.noreply.github.com>
2025-08-28 13:05:59 +0100

    Update TODO.R

--- TODO.R
+++ TODO.R
@@ -1,17 +1,20 @@
 # Tab 1
 # Sidebar: add a drop down options for year_class_selector exclude Sub-Junior and Junior and vice versa (maybe three-way switch)
 # Sidebar: add references for the data
-# Sidebar add a selector for federation
+# Sidebar: check that options "All" in dropdown selector are in capital
+# Sidebar: add filter by year of the comps
 # Plots: x-lim for lift plots so they fit  min and max lifts
-# Plots: Add input value to the user lifts
-# Plots: Add line to show a user lifts on the density curve
-# Plots: Add quantile text on the plot
 # Plots: change colours of the plots for S, B, D
-# Plots: Add Total lifts plot
-# Bottom text area: add count(n) of both 3 lifts
+# Plots: fix annotation overlapping with axis
+# Plots: add options for plotly to zoom fullscreen
+# Plots: remove control panel and zoom options
+# Plots: fix hover text
+# Bottom text area: Add count(n) of both 3 lifts
 # Bottom text area: Add brief description of the tab
+# Bottom text area: Add explanation of Quantile
 
-# Tab 2: Lifts selector - when user select lift what is the most popular choice is for the next lift etc
+# Tab 2
+# Lifts selector - when user select lift what is the most popular choice is for the next lift etc
 # Bottom text area: Add brief description of the tab
 
 shiny::runApp("C:/GIT/shinyIPF/")
@@ -23,11 +26,3 @@ rsconnect::setAccountInfo(
 )
 
 rsconnect::deployApp('C:/GIT/shinyIPF/')
-
-fed_choices <- as.character(unique(ipf_df$federation))
-shiny::selectizeInput(
-  inputId = "federation_selector",
-  label = "Federation",
-  choices = c("All", fed_choices),
-  options = list(placeholder = 'Type to search...')
-)
